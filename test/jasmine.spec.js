de80fc4ebd71c07620836e499ac8bfac226c5740
toran billups <toranb@gmail.com>
2014-06-09 21:06:13 -0500

    added final async (done) helper/assertion

--- test/jasmine.spec.js
+++ test/jasmine.spec.js
@@ -1,5 +1,9 @@
 describe("hello", function() {
-    it("jasmine", function() {
+    it("jasmine", function(done) {
+        setTimeout(function() {
+            expect(1).toBe(2);
+            done();
+        }, 1000);
         expect(1).toBe(1);
     });
 });
--- test/mocha.spec.js
+++ test/mocha.spec.js
@@ -2,7 +2,11 @@ var chai = require('chai');
 var expect = chai.expect;
 
 describe("hello", function() {
-    it("mocha", function() {
+    it("mocha", function(done) {
+        setTimeout(function() {
+            expect(1).eql(2);
+            done();
+        }, 1000);
         expect(1).eql(1);
     });
 });
