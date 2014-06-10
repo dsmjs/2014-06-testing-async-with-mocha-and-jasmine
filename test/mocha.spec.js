28452f756f2df88ba455f5d3afc5db14609dada8
toran billups <toranb@gmail.com>
2014-06-09 20:58:40 -0500

    pulled done (for the demo-more dramatic)

--- test/mocha.spec.js
+++ test/mocha.spec.js
@@ -3,10 +3,6 @@ var expect = chai.expect;
 
 describe("hello", function() {
     it("mocha", function() {
-        // setTimeout(function() {
-        //     expect(1).eql(2);
-        //     done();
-        // }, 250);
         expect(1).eql(1);
     });
 });
